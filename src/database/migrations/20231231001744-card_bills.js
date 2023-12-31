c60d02af434a5d6998695f58aba7faf9f1cd65f1
rafaelFacundo <rafaelfacundo63@gmail.com>
2023-12-30 21:26:46 -0300

    card bills migration done

--- src/database/migrations/20231231001744-card_bills.js
+++ src/database/migrations/20231231001744-card_bills.js
@@ -1,22 +1,48 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    return queryInterface.createTable("card_bills", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "users", key: "id" },
+      },
+      amount: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      expiration_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      is_paid: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+    return queryInterface.dropTable("card_bills");
+  },
 };
