ea88136fa30a4cb8b032d4f0493022393f6263c8
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-01-01 17:07:53 -0300

    insert in table users done, need to finish all the signup process

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,41 @@
 const User = require("../database/model/User");
 const { encryptPassword, comparePassword } = require("../utils/cryptography");
+const { DATEONLY } = require("sequelize");
 
 const createNewUser = async (req, res) => {
-  const { name, email, password, birth_date } = req.body;
-
-  const passwordHash = await encryptPassword(password);
-
-  const user = await User.create({
+  const {
     name,
     email,
-    passwordHash,
+    password,
     birth_date,
-    is_active: true,
-  });
+    document,
+    city,
+    neighbourhood,
+    zip,
+  } = req.body;
 
-  delete user.password;
+  try {
+    const passwordHash = await encryptPassword(password);
 
-  return res.status(200).json({ user });
+    /* console.log("a data de nascimento é ");
+    console.log(new Date(birth_date).g); */
+
+    console.log("O HASH DA SENAH È ", passwordHash);
+
+    const user = await User.create({
+      name,
+      email,
+      password: passwordHash,
+      birth_date: new Date(birth_date),
+      is_active: true,
+    });
+
+    delete user.password;
+    return res.status(200).json({ res: user });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ res: "SOMETHING WENT WRONG" });
+  }
 };
 
 module.exports = {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,12 +2,6 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const userRouter = express.Router();
 
-userRouter.post("/user/signup", (req, res) => {
-  console.log("The data that we received were");
-  console.log(req.body);
-
-  console.log("I'm gonna answer the request");
-  res.status(200).json({ res: "THIS YEAR IS THE YEAR" });
-});
+userRouter.post("/user/signup", userController.createNewUser);
 
 module.exports = userRouter;
