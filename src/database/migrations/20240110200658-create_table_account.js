87125fd3df6266d2ed985043d0c9f4dafecced50
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-01-10 17:28:19 -0300

    account table created and fixed

---
+++ src/database/migrations/20240110200658-create_table_account.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable("account", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "users", key: "id" },
+      },
+      account_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      amount: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable("account");
+  },
+};
---
+++ src/database/migrations/20240110202048-fixing_account_table.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn("account", "created_at", {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }),
+      queryInterface.addColumn("account", "updated_at", {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }),
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([
+      queryInterface.removeColumn("account", "created_at"),
+      queryInterface.removeColumn("account", "updated_at"),
+    ]);
+  },
+};
