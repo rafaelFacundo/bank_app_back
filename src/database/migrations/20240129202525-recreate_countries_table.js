67098c22319d9c3f9d0ed590c9303ec5adeb1a86
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-01-29 17:36:07 -0300

    countries table recreated

---
+++ src/database/migrations/20240129202525-recreate_countries_table.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable("countries", {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      code: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      currency: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable("countries");
+  },
+};
