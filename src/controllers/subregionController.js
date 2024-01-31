a3db1f23f0fc4aa305677d70a0a91cabca1d8bd9
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-01-30 21:32:47 -0300

    doing addresses releated controllers - doing subregions get all

---
+++ src/controllers/subregionController.js
@@ -0,0 +1,8 @@
+const Subregion = require("../database/model/Subregion.js");
+
+const getAllSubregions = (req, res) => {
+  try {
+  } catch (error) {
+    return res.status(500).json({ res: "SOMETHING WENT Wrong" });
+  }
+};
---
+++ src/routes/SubregionRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const SubregionRouter = express.Router();
+
+module.exports = SubregionRouter;
