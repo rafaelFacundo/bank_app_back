253c7b2fa8d71e2afb59730ff6f77c2972a9e370
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-02-05 22:00:59 -0300

    doing amount transfer controller, need to test the new createUser and delete all the files releated to Cards

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,5 +1,7 @@
 const Account = require("../database/model/Account");
+const databaseConnection = require("../database/index");
 
+//this function is used in another function that is located in the userController
 export const createAccount = async (userId, amount = 265.0) => {
   try {
     const response = await Account.create({
@@ -10,5 +12,32 @@ export const createAccount = async (userId, amount = 265.0) => {
   } catch (error) {
     console.log(error);
     throw new Error("something went wrong in creating the account");
+  }
+};
+
+export const transferAmount = async (req, res) => {
+  const newTransaction = await databaseConnection.transaction();
+  const { userSenderId, userReceiverId, amountToSend } = req.body;
+  try {
+    //getting the acoount of the user that is sending the amount to first verify
+    //if the he has the necessary amount to send
+    const userSenderAccount = await Account.findOne({
+      where: { user_id: userSenderId },
+    });
+
+    //if not return an error response
+    if (userSenderAccount.amount < amountToSend) {
+      return res.status(500).json({
+        res: "YOUR AMOUNT IS SMALLER THAN THE AMOUNT YOU WANT TO SEND.",
+      });
+    }
+
+    const userReceiverAccount = await Account.findOne({
+      where: {
+        user_id: userReceiverId,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ res: "SOMETHING WENT WRONG." });
   }
 };
