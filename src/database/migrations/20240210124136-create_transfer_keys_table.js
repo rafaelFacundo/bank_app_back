478bd28fd5a85092b3d1a3e977649c09116b82b1
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-02-10 09:49:46 -0300

    created tables - transactions, keys and contacts

---
+++ src/database/migrations/20240210124136-create_transfer_keys_table.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable("keys", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user: {
+        type: Sequelize.INTEGER,
+        references: { model: "users", key: "id" },
+        allowNull: false,
+      },
+      key: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable("keys");
+  },
+};
